39a8763cbcc871772fdd1c04c16e5fa81ebf91e5
Bill Leddy <bill@williesworkshop.net>
2016-01-09 14:13:07 -0800

    remove the local contact/config.php from version control

--- contact/config.sample.php
+++ contact/config.sample.php
@@ -1,16 +1,27 @@
 
 <?php 
 // Set the path to global settings 
-$site['config_library'] = $_SERVER['DOCUMENT_ROOT'].'../lib'; // always one dir down from Document Root
+//$site['config_library'] = $_SERVER['DOCUMENT_ROOT'].'../lib'; // this fails on some versions of php?
+$site['config_library'] = '/Users/baw/Sites/lib'; // use absolute path to be sure...
 $site['config_global_settings'] = $site['config_library'].'/config.php'; // Inside the config_library dir
+$site['mailer_class'] = $site['config_library'].'/MailClass.inc'; // Inside the config_library dir
 
 // get system wide settings 
-if((file_exists($site['config_global_settings'])) && (file_exists($site['config_library'])) && (file_exists($site['config_library'].'/MailClass.inc'))) {
+if(file_exists($site['config_library'])) {
+    // path is bad
+} else {
+    throw(new Exception("Global Mail Library directory do not exist"));
+}
+if(file_exists($site['config_global_settings'])) {
     require_once($site['config_global_settings']);
-    // Grab the FreakMailer class 
-    require_once($site['config_library'].'/MailClass.inc'); 
 } else {
     throw(new Exception("Global Mail Configuration Files do not exist"));
+}
+if(file_exists($site['mailer_class'])) {
+    // Grab the FreakMailer class 
+    require_once($site['mailer_class']); 
+} else {
+    throw(new Exception("Mailer Class Files do not exist"));
 }
 
 // Configuration settings for My Site 
