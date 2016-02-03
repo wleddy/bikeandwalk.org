d26d18a9c276b6cf9a2387e98fb65882d3d423e8
Bill Leddy <bill@williesworkshop.net>
2016-02-03 13:46:10 -0800

    reorganize help pages

--- help/count_administration/lessons/User_Records/index.php
+++ help/count_administration/lessons/User_Records/index.php
@@ -3,9 +3,9 @@
 $pageTitle = 'User Records';
 
 ## Add items to the header if needed
-$extraCSS = '<link rel="stylesheet" href="../../count_administration.css" type="text/css" media="all" >';
+##$extraCSS = '<link rel="stylesheet" href="../../count_administration.css" type="text/css" media="all" >';
 #$extraHeaders = '';
-$extraJS = '<script type="text/javascript" src="../../count_administration.js" ></script>';
+##$extraJS = '<script type="text/javascript" src="../../count_administration.js" ></script>';
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/templates/base.php'); 
 
